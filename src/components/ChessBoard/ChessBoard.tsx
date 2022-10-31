c9808dbad83bc5a392002d6416b763a0bb22226d
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-10-31 17:27:37 -0300

    pieces collision system finished

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -1,13 +1,50 @@
-import { MouseEvent, useRef } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import "./ChessBoard.css"
 
 import Tile from "../Tile/Tile";
 
-
+const initialPosition =
+  {  
+    a1: "url(/images/rook_w.png)",
+    h1: "url(/images/rook_w.png)",
+    b1: "url(/images/knight_w.png)",
+    g1: "url(/images/knight_w.png)",
+    c1: "url(/images/bishop_w.png)",
+    f1: "url(/images/bishop_w.png)",
+    d1: "url(/images/queen_w.png)",
+    e1: "url(/images/king_w.png)",
+    a8: "url(/images/rook_b.png)",
+    h8: "url(/images/rook_b.png)",
+    b8: "url(/images/knight_b.png)",
+    g8: "url(/images/knight_b.png)",
+    c8: "url(/images/bishop_b.png)",
+    f8: "url(/images/bishop_b.png)",
+    d8: "url(/images/queen_b.png)",
+    e8: "url(/images/king_b.png)",
+    a2: "url(/images/pawn_w.png)",
+    b2: "url(/images/pawn_w.png)",
+    c2: "url(/images/pawn_w.png)",
+    d2: "url(/images/pawn_w.png)",
+    e2: "url(/images/pawn_w.png)",
+    f2: "url(/images/pawn_w.png)",
+    g2: "url(/images/pawn_w.png)",
+    h2: "url(/images/pawn_w.png)",
+    a7: "url(/images/pawn_b.png)",
+    b7: "url(/images/pawn_b.png)",
+    c7: "url(/images/pawn_b.png)",
+    d7: "url(/images/pawn_b.png)",
+    e7: "url(/images/pawn_b.png)",
+    f7: "url(/images/pawn_b.png)",
+    g7: "url(/images/pawn_b.png)",
+    h7: "url(/images/pawn_b.png)"
+  } 
 
 export default function ChessBoard() {
 
+
   const chessBoardRef = useRef<HTMLDivElement>(null);
+
+  const [pieces, setPieces] = useState(initialPosition);
 
   let activePiece: HTMLElement | null = null;
 
@@ -64,46 +101,34 @@ export default function ChessBoard() {
   }
 
   const dropPiece = (event: MouseEvent) => {
+
+    //Casilla donde estaba la pieza
+    const target = event.target as HTMLDivElement
+    const previusTile = target.parentElement?.id;
+    //console.log(previusTile);
+
+    //casilla donde queda
+    const x = event.clientX, y = event.clientY;
+    const elementMouseIsOver = document.elementsFromPoint(x, y);
+    const newTile = elementMouseIsOver[elementMouseIsOver.length - 6].id
+    //console.log(newTile)
+
+    
     if (activePiece) {
       activePiece = null;
     }
+
+    
+    setPieces(
+      {
+        ...pieces,
+        [newTile as keyof typeof pieces]: pieces[previusTile as keyof typeof pieces],
+        [previusTile as keyof typeof pieces]: ""
+      }
+    )
   }
 
-  const piecePosition =
-  {  
-    a1: "url(/images/rook_w.png)",
-    h1: "url(/images/rook_w.png)",
-    b1: "url(/images/knight_w.png)",
-    g1: "url(/images/knight_w.png)",
-    c1: "url(/images/bishop_w.png)",
-    f1: "url(/images/bishop_w.png)",
-    d1: "url(/images/queen_w.png)",
-    e1: "url(/images/king_w.png)",
-    a8: "url(/images/rook_b.png)",
-    h8: "url(/images/rook_b.png)",
-    b8: "url(/images/knight_b.png)",
-    g8: "url(/images/knight_b.png)",
-    c8: "url(/images/bishop_b.png)",
-    f8: "url(/images/bishop_b.png)",
-    d8: "url(/images/queen_b.png)",
-    e8: "url(/images/king_b.png)",
-    a2: "url(/images/pawn_w.png)",
-    b2: "url(/images/pawn_w.png)",
-    c2: "url(/images/pawn_w.png)",
-    d2: "url(/images/pawn_w.png)",
-    e2: "url(/images/pawn_w.png)",
-    f2: "url(/images/pawn_w.png)",
-    g2: "url(/images/pawn_w.png)",
-    h2: "url(/images/pawn_w.png)",
-    a7: "url(/images/pawn_b.png)",
-    b7: "url(/images/pawn_b.png)",
-    c7: "url(/images/pawn_b.png)",
-    d7: "url(/images/pawn_b.png)",
-    e7: "url(/images/pawn_b.png)",
-    f7: "url(/images/pawn_b.png)",
-    g7: "url(/images/pawn_b.png)",
-    h7: "url(/images/pawn_b.png)"
-  } 
+  
 
   const horizontalAxis = [8, 7, 6, 5, 4, 3, 2, 1]
   const verticalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
@@ -128,7 +153,7 @@ export default function ChessBoard() {
 
       {algebraicNotation.map((element, key) => 
 
-        <Tile key={key} image={piecePosition[element as keyof typeof piecePosition]}>
+        <Tile key={key} image={pieces[element as keyof typeof pieces]}>
           {element}
         </Tile>
   
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from "react";
+//import { ReactNode } from "react";
 import "./Tile.css";
 
 type TileProps = {
   image: string;
-  children: ReactNode;
+  children: string;
 };
 
 export default function Tile({ children, image }: TileProps) {
 
 
   return (
-    <div className="tile">
+    <div className="tile" id={children}>
 
       {image && <div className="piece" style={{backgroundImage: `${image}`}}></div>}
       
